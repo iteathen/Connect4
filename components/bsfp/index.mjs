bf4f120a48509eb0755245dc58052bfdad456764
iteathen <josh.oshiro@gmail.com>
2026-09-09 21:29:52 -0700

    Export BSFP support-lattice seam

---
+++ components/bsfp/index.mjs
@@ -0,0 +1,7 @@
+export {
+  BSFP_INVALID_ITEM_U32,
+  BSFP_4X3_CONNECT3_SUPPORT,
+  createBsfpSupportLatticeProfile,
+} from './support-lattice.mjs';
+
+export { createBsfpSupportPredecessorDeviceLibraryRequest } from './device/support-predecessor-library.mjs';
